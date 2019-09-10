4e1059ce3c86f4f3b6ae435fdb238df34fd39889
Nitish Mhaske <35861324+NitishMhaske@users.noreply.github.com>
2019-09-10 15:11:57 +0530

    Create ForLoop.c

---
+++ ForLoop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void Display(){
+    printf("Nitish\n");
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        Display();
+    }
+    return 0;
+}
